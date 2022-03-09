6898094f0306b4e5bf7e750747453efa659af15d
AndyRama <andyramaroson@gmail.com>
2022-03-09 21:44:26 +0100

    [enhancement] homepage add average daily rate for freelance

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,30 +1,37 @@
 function photographerFactory(data) {
-    const { name, portrait, city, country, tagline } = data;
+  const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
+	function getUserCardDOM() {
+		const article = document.createElement('article');
+		const img = document.createElement('img');
+		img.setAttribute("src", picture)
+		const h2 = document.createElement('h2');
+		h2.textContent = name;
 
-        //ajout city
-        const hCity = document.createElement('h3');
-        hCity.classList.add('city');
-        hCity.textContent = `${city}, ${country}`; 
+		//add city & country
+		const hCity = document.createElement('h3');
+		hCity.classList.add('city');
+		hCity.textContent = `${city}, ${country}`; 
 
-        //ajout tagline
-        const pTagLine = document.createElement('p');
-        pTagLine.classList.add('tagline');
-        pTagLine.textContent = tagline; 
+		//add tagline
+		const pTagLine = document.createElement('p');
+		pTagLine.classList.add('tagline');
+		pTagLine.textContent = tagline; 
 
-        article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(hCity);
-				article.appendChild(pTagLine);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+		//add price
+		const sPrice = document.createElement('span');
+		sPrice.classList.add('price');
+		sPrice.textContent = `${price}€/jour`;
+
+		article.appendChild(img);
+		article.appendChild(h2);
+		article.appendChild(hCity)
+		article.appendChild(pTagLine);
+		article.appendChild(sPrice);
+
+		return (article);
+	}
+	return { name, picture, getUserCardDOM }
 }
