c9355d79fe3651899360748bbae2b9b688d38dce
AndyRama <andyramaroson@gmail.com>
2022-03-09 12:27:23 +0100

    [enhancement] form-field submit firstname with error

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -6,20 +6,54 @@ const lastName = document.getElementById("lastName");
 const lastError = document.getElementById("lastError");
 const mail = document.getElementById("email");
 const emailError = document.getElementById("emailError");
+const formField= document.getElementById("form-field");
+
+// launch modal event
+function displayModal() {
+  const modal = document.getElementById("contact_modal");
+  modal.style.display = "block";
+}
+
+//Close modal 
+function closeModal() {
+  const modal = document.getElementById("contact_modal");
+  modal.style.display = "none";
+}
 
 // Reset form error
+const clearInput = () => {
+  //remove input field
+  form.reset();
+
+  //remove error form
+  const resetErrorBorder = document.querySelectorAll("input")
+  resetErrorBorder.forEach((element) => {
+      element.classList.remove("errorForm");
+  });
+
+  //remove error message
+  const resetErrorMessage = document.querySelectorAll(".result")
+  resetErrorMessage.forEach((message) => {
+      message.innerHTML = "";
+  });
+};
 
 //Check validation for firstname
 function validationFirst () {
+  //
+  firstError.innerHTML = "";
+  firstError.classList.remove("errorStyle");
+  firstName.classList.remove("errorForm");
+
   if (firstName.value.trim().length == 0) {
-    firstError.innerHTML = 'Veuillez siasir votre prénom.';
+    firstError.innerHTML = "Veuillez siasir votre prénom.";
     firstError.classList.add("errorStyle");
-    firstName.classList.add('errorForm');
+    firstName.classList.add("errorForm");
     return false;
   } else if (firstName.value.trim().length < 2) {
-    firstError.innerHTML = 'Veuillez entrer 2 caractères ou plus pour le champ du prénom.';
-    firstError.classList.add('errorStyle');
-    firstName.classList.add('errorForm');
+    firstError.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+    firstError.classList.add("errorStyle");
+    firstName.classList.add("errorForm");
     return false;
   } else {
     firstError.innerHTML = "";
@@ -30,14 +64,21 @@ function validationFirst () {
 //Check validation for lastname
 //Check validation for Email
 
-// launch modal event
-function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
-}
+//Listen for events in each input and launch the associated function 
+firstName.addEventListener("input", validationFirst);
 
-//Close modal 
-function closeModal() {
+//Sending the forms 
+formField.addEventListener("submit", (event) => {
+  event.preventDefault();
+  validationFirst();
+
+  //Check if all conditions are met 
+  if (validationFirst() == true ) {
+    //If everything is good, we remove the form 
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
-}
+    clearInput()
+  } else {
+    return false;
+  }
+})
