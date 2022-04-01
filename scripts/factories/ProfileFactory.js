07ac94ab685dcc338aa0252fd52cbec770f03909
AndyRama <andyramaroson@gmail.com>
2022-04-01 19:35:36 +0200

    [enhancement] dropdown orderBtn => 50%

--- scripts/factories/ProfileFactory.js
+++ scripts/factories/ProfileFactory.js
@@ -1,8 +1,13 @@
 function profileFactories(photographeObject) {
-  const { name, city, country, tagline, portrait, id, medias, price, video, image} = photographeObject;
+  const { name, city, country, tagline, portrait, id, medias, price, video, image, likes} = photographeObject;
   const thumbs = `assets/photographers/${portrait}`;
   const picture = `assets/photos/${image}`;
   const pictureVideo = `assets/photos${video}`;
+  let currentPhotographerPhotos = [];
+  let currentLigthboxIndex = -1;
+  let likesTable = [];
+  let existingLikes = [];
+  let modifiedArray = [];
 
   function fillPagePhotographe() {    
     //Thumbs    
@@ -30,25 +35,21 @@ function profileFactories(photographeObject) {
 		photographeName.innerHTML = name;
 
     //Add likes
-    function addLikes () {
+    function addLikes (photographeObject) {
       // Cible tous les coeur et ecoute les clicks
+      media = medias.likes;
       const heart = document.querySelectorAll(".heart");
       heart.forEach((icon) => {
         icon.addEventListener("click", () => {
           // A chaque click, on ajoute un +1 au chiffre de like et aux nb total de like
-          totalLike += 1
-          nbLikes += 1
+          totalLike += 1;
           // Charge et affiche le nouveau chiffre dans "total"
           const nblikes = document.getElementById("nbLikes"); 
-          nblikes.innerHTML = totalLike;     
-          console.log(totalLike);
-
-           // Charge et affiche le nouveau chiffre dans "counterLikeCard"
-          const addlikes = document.querySelectorAll(".card-counter"); 
-          // addlikes = nbLikes;
-          // addlikes.innerHTML = addlikes ;    
-          console.log(nbLikes);
-
+          nblikes.innerHTML = totalLike;   
+          // media = medias.likes;
+        
+          //MISSING ADD LIKE CARDS
+          
         }, { once: true }) // N'autorise qu'un click
       })
     }
@@ -56,12 +57,14 @@ function profileFactories(photographeObject) {
     //display media video/image in profile
     let nbLike = 0;
     medias.map(media => {
-      const cardContainer = document.querySelector('.card-container');
-      // console.log(photographeObject.medias);
       nbLike += media.likes;
+      const cardContainer = document.querySelector('.card-container');
+      // console.log(photographeObject.medias);     
+
       let photoToDisplay;
       if(media.image){ 
         photoToDisplay = media.image;   
+             
         //return card image with model
         const mediaCardImage = `
             <div class="cards" >      
@@ -99,6 +102,7 @@ function profileFactories(photographeObject) {
 
     //display media video/image in lightBox
     const lightContainer = document.querySelector('.lightBoxContainer');
+    // lightContainer.display.style = "background-color: rgba(192, 189, 189, 0.5)";
     medias.map(media => {
       let photoToDisplayBox;   
 
@@ -134,6 +138,43 @@ function profileFactories(photographeObject) {
   let totalLike = nbLike;
   document.getElementById("nbLikes").innerText = nbLike;
   addLikes();
+  trier();
+  }
+  
+// btn Order
+function trier() {
+  const btnOrder = Array.from(document.getElementsByClassName('trierBtn'));
+  btnOrder.forEach((btn, index) => btn.addEventListener('click', () => {
+    
+    if( index == 0) {
+      // sort by POPULARITY   
+      modifiedArray = medias.sort((a, b) => {return b.likes - a.likes})
+      console.log(modifiedArray);
+      currentPhotographerPhotos = modifiedArray.push(currentPhotographerPhotos);
+      const newCard = document.querySelector(".card-container").innerHTML = "";
+      // INSERER NOUVEAUX TABLEAU
+
+    } else if (index == 1) {
+
+      // sort by DATE 
+      modifiedArray = photographeObject.medias.sort((a, b) => { return new Date(a.date).valueOf() - new Date(b.date).valueOf();}) 
+      console.log(modifiedArray);
+      document.querySelector(".card-container").innerHTML = "";
+      // INSERER NOUVEAUX TABLEAU
+    } else if ( index == 2) {
+
+      //sort by ALFABETIC ORDER
+     const title = medias.title 
+      modifiedArray = photographeObject.medias.sort((a, b) => {
+      if(a.title.toLowerCase() < b.title.toLowerCase()) { return -1;}
+      else if (a.title.toLowerCase() > b.title.toLowerCase()) {return 1;}
+      })
+      console.log(modifiedArray);
+      document.querySelector(".card-container").innerHTML = "";
+      // INSERER NOUVEAUX TABLEAU
+      }
+    }));
   }
   return { name, thumbs, picture, pictureVideo, fillPagePhotographe}
 }
+
--- scripts/utils/dropdown.js
+++ scripts/utils/dropdown.js
@@ -15,23 +15,3 @@ document.getElementById("close-up-icon").addEventListener('click', () => {
   chevronUpIcon.classList.add("fa-chevron-up-none");
   chevronDownIcon.classList.toggle("fa-chevron-up-none");
 })
-
-
-//OPEN DropDown
-  document.getElementById('drop-down-btn').addEventListener('click', () => {
-    const hidenPart = document.getElementById("myDropdown");
-    const chevronUpIcon = document.getElementById("close-up-icon");
-    const chevronDownIcon = document.getElementById('drop-down-btn');  
-        hidenPart.classList.add("show");//add show class to change display by default which is none
-        chevronUpIcon.classList.remove("fa-chevron-up-NO");//remove this class which gives display none by default
-        chevronDownIcon.classList.toggle("fa-chevron-up-NO");
-  })
-// CLOSE DropDown
- document.getElementById("close-up-icon").addEventListener('click', () => {
-   const hidenPart = document.getElementById("myDropdown");
-   const chevronUpIcon = document.getElementById("close-up-icon");
-   const chevronDownIcon = document.getElementById('drop-down-btn');
-        hidenPart.classList.remove("show");
-        chevronUpIcon.classList.add("fa-chevron-up-NO");
-        chevronDownIcon.classList.toggle("fa-chevron-up-NO");
- })
